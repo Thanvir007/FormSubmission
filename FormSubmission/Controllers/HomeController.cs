2162904e772e7d8c8c8e6760d0d00a8a44d02329
User <User@DESKTOP-GNGOOHN>
2024-02-14 21:58:03 +0600

    Add project files.

---
+++ FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Student()
+        {
+
+            List<Student> stdList = new List<Student>();
+            Student std = new Student();
+
+            std.ID = "20-12345-2";
+            std.Name = "Thanvir Ahmed";
+            std.UserName = "thanvirahmed";
+            std.Email = "20-12345-2@student.aiub.edu";
+
+            stdList.Add(std);
+
+
+            return View(stdList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FormSubmission/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class Student
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z- ]{4,30}$")]
+        [StringLength(30, MinimumLength =4, ErrorMessage = "Please Enter Your Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{2}-[0-9]{5}-[1-3]{1}$")]
+        [StringLength(30, MinimumLength = 4, ErrorMessage = "Please Enter Valid ID (Example: XX-XXXXX-X)")]
+        public string ID { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z-]{3,10}$")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Please Enter UserName Between 3 and 10 Character")]
+        public string UserName { get; set; }
+
+        [RegularExpression(@"^[0-9]{2}-[0-9]{5}-[1-3]{1}@[a-zA-Z.]{16}$")]
+        [Display(Name = "Please Enter Your Student Email Address (Example: XX-XXXXX-X@Student.aiub.edu)"), Required]
+        public string Email { get; set; }
+
+
+    }
+}
